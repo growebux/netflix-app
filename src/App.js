0cbcca4f8f1031378c50cbc012d2475f5606abf5
Rafa <rafa@Rafas-MacBook-Pro.local>
2021-12-09 00:14:42 -0800

    Add useState and useEffect, fetch the imagens from the API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
+import react from 'react';
 import './App.css';
+import Row from './row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <div>hello</div>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }

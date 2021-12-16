e35504064fa360248ab557b6cd479e824e070e32
Rafa <rafa@Rafas-MacBook-Pro.local>
2021-12-15 23:43:40 -0800

    Update Banner

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import react from 'react';
-import './App.css';
-import Row from './row';
-import requests from './requests';
-import Banner from './banner'
-import Nav from './nav';
-import './nav.css'
-
-
+import react from "react";
+import "./App.css";
+import Row from "./row";
+import requests from "./requests";
+import Banner from "./banner";
+import Nav from "./nav";
+import "./nav.css";
+import React from "react";
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      {/* NavBar */}
-      {/* Banner */}
       <Nav />
       <Banner />
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}isLargeRow />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies}/>
-      
+     
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   );
 }

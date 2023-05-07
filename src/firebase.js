ee8a6f0c8d1925477e8524bccf94690619cc5d6a
Rafael Lopes <rafaellopes@Rafaels-MacBook-Pro.local>
2023-05-06 17:58:02 -0700

    Replace API from TMDB

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAWPROHzrzyCyzLf4d45lWWv1pjJ2ZVdO4",
-    authDomain: "netflix-app-9860f.firebaseapp.com",
-    projectId: "netflix-app-9860f",
-    storageBucket: "netflix-app-9860f.appspot.com",
-    messagingSenderId: "218680989667",
-    appId: "1:218680989667:web:091c1af468e14db0abf2d6",
-    measurementId: "G-DXPWP0G2J3"
-  };
+  apiKey: "AIzaSyAWPROHzrzyCyzLf4d45lWWv1pjJ2ZVdO4",
+  authDomain: "netflix-app-9860f.firebaseapp.com",
+  projectId: "netflix-app-9860f",
+  storageBucket: "netflix-app-9860f.appspot.com",
+  messagingSenderId: "218680989667",
+  appId: "1:218680989667:web:091c1af468e14db0abf2d6",
+  measurementId: "G-DXPWP0G2J3",
+};
